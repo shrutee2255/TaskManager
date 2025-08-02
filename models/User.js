440edf2eacc687fff54d397a85c42e2e0ddb070a
shrutee2255 <146308027+shrutee2255@users.noreply.github.com>
2025-08-02 22:51:34 +0530

    Initial commit

--- models/User.js
+++ models/User.js
@@ -30,7 +30,6 @@ userSchema.pre('save',async function(){
     const salt = await bcrypt.genSalt(10)
     this.password = await bcrypt.hash(this.password,salt)
    
-
 })
 
 userSchema.methods.createJWT = function(){
