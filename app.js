2a26cdb3ec6b263b9471aaa126210f6bf681952f
shrutee2255 <146308027+shrutee2255@users.noreply.github.com>
2025-08-04 23:29:55 +0530

    Removed certs file from Repo and used https

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 require('dotenv').config();
 require('express-async-errors');
 const express = require('express');
+const https = require('https')
 const app = express();
+const fs = require('fs')
+
+const tlsSessionCache = new Map();
 
 // connectDB
 const connectDB =require('./db/connect')
@@ -33,12 +37,37 @@ app.use('/api/v1/jobs',authenticateUser,jobsRouter)
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
+const option={
+  key:fs.readFileSync('./certs/key.pem'),
+  certs:fs.readFileSync('./certs/certs.pem'),
+
+  resumesession(sessionid,callback){
+    const session = tlsSessionCache.get(sessionid.toString('hex'));
+    callback(null,session)
+  }
+
+}
+const server = https.createServer(option,app);
+
+server.on('newSession',(sessionid,sessindata,callback)=>{
+  try{
+    tlsSessionCache.set(sessionid.toString('hex'),sessiondata);
+  }
+  catch(err){
+    callback(err)
+
+  }
+})
+
+
+
 const port = process.env.PORT || 5000;
 
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI)
-    app.listen(port, () =>
+    console.log(process.env.MONGO_URI)
+    server.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
   } catch (error) {
